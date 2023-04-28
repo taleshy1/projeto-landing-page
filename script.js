87281b07b87c7817ef8e3fcda2f19069cd8dd405
Tales Patric <taleshy1@gmail.com>
2023-04-27 22:47:17 -0300

    Projeto landingpage finalizado

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-// window.onbeforeunload = () => {
-//   window.scrollTo(0, 0);
-// };
+window.onbeforeunload = () => {
+  window.scrollTo(0, 0);
+};
 
 document.addEventListener("DOMContentLoaded", () => {
   const animatedItem = document.querySelector(".logo-camiao");
@@ -27,4 +27,53 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   window.addEventListener("scroll", update);
+  
+  
 });
+
+
+
+function enviarForm(event) {
+  
+  event.preventDefault();
+  const nome = document.getElementById("name")
+  const email = document.getElementById("email")
+  const assunto = document.getElementById("assunto")
+  const textarea = document.getElementById("textarea")
+  console.log(nome, email, assunto, textarea)
+
+  alert(`Você clicou no botão de Enviar Formulario!\nOs conteúdos recebidos foram\nNome: ${nome.value}\nEmail: ${email.value}\nAssunto: ${assunto.value}\nMensagem: ${textarea.value}`)
+  nome.value = ""
+  email.value = ""
+  assunto.value = ""
+  textarea.value = ""
+}
+
+
+function login (event) {
+  event.preventDefault()
+  alert(`Você clicou no botão de login`)
+}
+
+function register (event) {
+  event.preventDefault()
+  alert(`Você clicou no botão de Cadastre-se`)
+}
+
+function envieSeuProduto (event) {
+  event.preventDefault()
+  alert(`Você clicou no botão de Envie seu Produto`)
+}
+
+function trabalheConosco (event) {
+  event.preventDefault()
+  alert(`Você clicou no botão de Trabalhe Conosco`)
+}
+
+function rastrear (event) {
+  event.preventDefault()
+  const codigoRastreio = document.getElementById("track-code")
+  alert(`Você clicou no botão de Rastrear, o seu código de rastreio foi\n${codigoRastreio.value}`)
+  codigoRastreio.value = ""
+}
+
